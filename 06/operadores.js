97211f7b953cba623d80b23c85c0470d60a43bb2
vicco <victorcs2704@gmail.com>
2020-05-10 22:50:20 -0400

    06 Operadores Matematicos

---
+++ 06/operadores.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var number = 4
+var number2 = 5
+
+//Operadores basicos
+
+//suma
+var resultadoSuma = number + number2
+//resta
+var resultadoResta = number - number2
+//multiplicacion
+var resultadoMultiplicacion = number * number2
+//division
+var resultadoDivision = number / number2
+
+console.log(`El resultado de la suma es: ${resultadoSuma}`)
+console.log(`El resultado de la resta es: ${resultadoResta}`)
+console.log(`El resultado de la multiplicacion es: ${resultadoMultiplicacion}`)
+console.log(`El resultado de la division es: ${resultadoDivision}`)
+
+//Operadores de Incremento
+
+resultadoSuma ++
+console.log(`Incrementamos ++ ${resultadoSuma}`)
+
+resultadoSuma --
+console.log(`Decrementamos -- ${resultadoSuma}`)
