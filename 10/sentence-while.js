a8fcdec8f47b98f435436ab702b1762d864edcf4
vicco <victorcs2704@gmail.com>
2020-05-11 16:51:06 -0400

    10 Sentencia while

---
+++ 10/sentence-while.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Mientas que se cumpla la condicion, nos de true el Bucle se sigue ejecutando
+
+var date = 5
+
+while(date <=7) {
+    console.log(`El dato vale ${date}`)
+
+    date ++
+}
+console.log(`Ya se cumplió la condición`)
