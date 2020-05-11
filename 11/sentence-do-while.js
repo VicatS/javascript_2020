e7a4e54b1fe3d1b2938769202295609270faf730
vicco <victorcs2704@gmail.com>
2020-05-11 17:36:19 -0400

    11 Sentencia do while

---
+++ 11/sentence-do-while.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Mientas que se cumpla la condicion, nos de true el Bucle se sigue ejecutando
+
+var date = 1
+
+do {
+    console.log(`El valor de dato es ${date}`)
+    date ++ //IMPORTANTE
+}while (date <= 5)
+
+console.log(`Salimos del bucle sin colgar la PC`)
