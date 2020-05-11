3283767a111d3664eadafcb08dccf16d42991624
vicco <victorcs2704@gmail.com>
2020-05-10 23:29:40 -0400

    09 El bucle FOR

---
+++ 09/bucle-for.js
@@ -0,0 +1,12 @@
+'use strict'
+//Bucle for
+
+var i
+//Dentro del Bucle for tenemos 3 partes
+//Inicializacion    i = 0
+//Evalucacion       i <= 100
+//Iteracion         i++
+
+for (i = 1; i <= 100; i ++) {
+    console.log(i)
+}
