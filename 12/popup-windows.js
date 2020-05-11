51e8826a2b418deec82ca8001296251ac34f321f
vicco <victorcs2704@gmail.com>
2020-05-11 19:14:46 -0400

    12 Tipos de ventanas emergentes

---
+++ 12/popup-windows.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Alertas dentro del navegador
+alert(`Warning message`)
+
+//Ventas de confirmacion
+var result = confirm(`Confirm if you want to continue?`)
+alert(result)
+
+//Venta de ingreso de datos
+var res = prompt(`You want to continue?`, `Of course`)
+alert(`You said that ` + res)
