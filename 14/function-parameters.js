54646dc6d023520fd3899302874d67c243e537f5
vicco <victorcs2704@gmail.com>
2020-05-11 19:56:14 -0400

    15 Parametros opcional

--- 14/function-parameters.js
+++ 14/function-parameters.js
@@ -3,12 +3,24 @@
 var n1 = prompt(`Tell me a word`, `You have not put anything`)
 var n2 = prompt(`Tell me a word`, `You have not put anything`)
 
-function JoinWords(n1, n2) {
-    var PalabrasUnidas = `${n1} ${n2}`
+// function JoinWords(n1, n2, n3=false) {
+//     var PalabrasUnidas = `${n1} ${n2}`
+//
+//     return PalabrasUnidas
+// }
+//
+// var resultado = JoinWords(n1, n2)
+//
+// console.log(resultado)
 
-    return PalabrasUnidas
+
+function JoinWords(n1, n2, n3=false) {
+    if (n3 == false) {
+        console.log(`${n1} ${n2}`)
+    }else{
+        document.write(`${n1} ${n2}`)
+    }
 }
 
-var resultado = JoinWords(n1, n2)
-
-console.log(resultado)
+// JoinWords(n1, n2, false)
+JoinWords(n1, n2, true)
