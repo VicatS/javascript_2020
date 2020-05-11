67587ba1b257e34e542f8b7af031576121626a02
vicco <victorcs2704@gmail.com>
2020-05-11 19:26:30 -0400

    13 Funciones

---
+++ 13/functions.js
@@ -0,0 +1,10 @@
+'use strict'
+
+function NameFunction() {
+    return 'Hola soy el return de la primera function'
+}
+
+function ShowMessage() {
+    console.log(`Hola que tal estoy dentro de la function`)
+}
+ShowMessage();
