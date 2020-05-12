a65640a166b5afb8bc51004dc7cf635fa98275a3
Victor <victorcs2704@gmail.com>
2020-05-12 19:11:07 -0400

    Funciones anonimas

---
+++ 17/parameter-rest.js
@@ -0,0 +1,11 @@
+'use strict'
+
+//Parametros REST
+
+function ListadoPaises(p1,p2, ...p) {
+    console.log(p1);
+    console.log(p2);
+    console.log(p);
+}
+
+ListadoPaises('España', 'Alemani', 'Inglaterra', 'Suiza') 
