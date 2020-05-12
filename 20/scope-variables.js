a673d5283d0b27d80bb303449c2042778e30e677
Victor <victorcs2704@gmail.com>
2020-05-12 19:33:25 -0400

    Ambito de las variables

---
+++ 20/scope-variables.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var global = 'Variable Global'
+
+function MostrarConsola() {
+    var variable_function = 'Variable dentro de la funcion'
+    console.log(global);
+    console.log(variable_function);
+}
+
+MostrarConsola()
+
+console.log(variable_function);
