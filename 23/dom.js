f18265a3e202f920c3ba1400aa119f2691fa83c5
Victor <victorcs2704@gmail.com>
2020-05-12 21:04:55 -0400

    Doom

---
+++ 23/dom.js
@@ -0,0 +1,9 @@
+'use strict'
+
+var div1 = document.getElementById("div1")
+div1.innerHTML = 'Texto cambiado a traves de Javascript'
+div1.style.fontSize = '25px'
+
+var parrafo1 = document.getElementsByClassName("parrafo1")
+parrafo1[0].innerHTML = 'Texto cambiado'
+
