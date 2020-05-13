5b821e67dd8d57f127e14796925ffa995124feb1
Victor <victorcs2704@gmail.com>
2020-05-12 20:22:03 -0400

    Arrays

---
+++ 21/arrays.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Arrays
+
+var paises = ['España', 'Suiza', 'Alemania', 'Italia']
+var datos = [1,2,3, true, 'Coches']
+var cantidad_paises = paises.length
+
+console.log(paises);
+console.log(cantidad_paises);
+console.log(datos);
+
+
+
