d1c5d7dbff74b978cfbc83fbf0f045d5d379cced
victor <victorcs2704@gmail.com>
2020-05-14 22:50:17 -0400

    24 El BOOM

---
+++ 25 Eventos del raton/events-mouse.js
@@ -0,0 +1,19 @@
+'use strict'
+
+var boton = document.querySelector('#boton')
+var div = document.querySelector('#div')
+
+/*boton.addEventListener('click', function () {
+    console.log('As pulsado el boton')
+    div.innerHTML = 'As pulsado el boton'
+})*/
+
+function Pulsar() {
+    div.innerHTML = 'As pulsado el Boton'
+}
+
+function Alerta() {
+    alert('As pulsado el DIV')
+}
+
+//Averiguar sobre los metodos del raton al hacer click
