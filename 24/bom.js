1dd0f0fcf63815e0240d25ee1282e6ffd7cfa45b
victor <victorcs2704@gmail.com>
2020-05-14 22:24:35 -0400

    24 El BOOM

---
+++ 24/bom.js
@@ -0,0 +1,9 @@
+'use strict'
+
+//BOM BROWSER OBJECT MODEL
+console.log(window.innerHeight)
+console.log(window.innerWidth)
+console.log(screen.height)
+console.log(screen.width)
+
+console.log(window.location)
